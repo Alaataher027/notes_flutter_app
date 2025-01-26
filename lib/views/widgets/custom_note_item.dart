c418dea8f449285b53185610ea238a282ddad823
Alaataher027 <alaataher027@gmail.com>
2025-01-26 22:03:26 +0200

    + create edit note view

--- lib/views/widgets/custom_note_item.dart
+++ lib/views/widgets/custom_note_item.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:notes_app/views/edit_note_view.dart';
 
 class NoteItem extends StatelessWidget {
   final Color color;
@@ -8,53 +9,60 @@ class NoteItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
-      decoration: BoxDecoration(
-        color: color,
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            title: Text(
-              "Flutter Tips",
-              style: TextStyle(
-                fontSize: 26,
-                color: Colors.black,
-              ),
-            ),
-            subtitle: Padding(
-              padding: const EdgeInsets.symmetric(vertical: 16),
-              child: Text(
-                "description of the note",
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(context, MaterialPageRoute(builder: (context) {
+          return const EditNoteView();
+        }));
+      },
+      child: Container(
+        padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
+        decoration: BoxDecoration(
+          color: color,
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              title: Text(
+                "Flutter Tips",
                 style: TextStyle(
-                  color: const Color.fromARGB(182, 0, 0, 0),
-                  fontSize: 16,
+                  fontSize: 26,
+                  color: Colors.black,
+                ),
+              ),
+              subtitle: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 16),
+                child: Text(
+                  "description of the note",
+                  style: TextStyle(
+                    color: const Color.fromARGB(182, 0, 0, 0),
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+              trailing: IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  FontAwesomeIcons.trash,
+                  color: Colors.black,
+                  size: 25,
                 ),
               ),
             ),
-            trailing: IconButton(
-              onPressed: () {},
-              icon: Icon(
-                FontAwesomeIcons.trash,
-                color: Colors.black,
-                size: 25,
+            Padding(
+              padding: const EdgeInsets.only(right: 35),
+              child: Text(
+                "Sep 14, 2025",
+                style: TextStyle(
+                  fontSize: 16,
+                  color: const Color.fromARGB(182, 0, 0, 0),
+                ),
               ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(right: 35),
-            child: Text(
-              "Sep 14, 2025",
-              style: TextStyle(
-                fontSize: 16,
-                color: const Color.fromARGB(182, 0, 0, 0),
-              ),
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
